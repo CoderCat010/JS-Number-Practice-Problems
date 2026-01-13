2d08dafe58a6889dad90b767f870c1e0f951c97b
CoderCat010 <nusraumme@gmail.com>
2026-01-14 03:07:57 +0600

    added practice-8

--- basic-practice-problems.js
+++ basic-practice-problems.js
@@ -30,11 +30,27 @@
 
 
 //-----> Check if a number is even or odd.
-let evens_odds = 2;
-if(evens_odds % 2 === 0){
-    console.log('even');
+// let evens_odds = 2;
+// if(evens_odds % 2 === 0){
+//     console.log('even');
+// }else{
+//     console.log('odd');
+// }
+
+
+//-----> Find the largest of three numbers.
+let x = 3;
+let y = 55;
+let z = 5;
+
+if(x > y && x > z){
+    console.log('x is greater than y and z');
+}else if(y > x && y > z){
+    console.log('y is greater than x and z');
+}else if(z > x && z > y ){
+    console.log('z is greater than x and y');
 }else{
-    console.log('odd');
+    console.log('first provide a big number');
 }
 
 
